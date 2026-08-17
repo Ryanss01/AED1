59b1deba6e9a1f12501490df55eed74aec54a453
Ryanss01 <ryan.soares@alunos.ufersa.edu.br>
2026-08-17 20:05:55 -0300

    Ponteiro

---
+++ PONTEIRO.C
@@ -0,0 +1,13 @@
+int main (void){
+
+    int numero;
+    int* ponteiro;
+
+    printf("Digite um valor:");
+    scanf("%d", &numero);
+
+    ponteiro = &numero;
+    printf("Numero = %d \n ponteiro = &p");
+
+    return 0;
+}
